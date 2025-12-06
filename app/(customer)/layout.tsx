3caca398a623fbfbdc88fe5cbe74d363f33694ee
Huỳnh Trọng Nhân <142094267+NhanItJAVA1@users.noreply.github.com>
2025-12-06 17:05:11 +0700

    create page: (product)/page.tsx (product)/[id]/page.tsx component/ProductCard

--- app/(customer)/layout.tsx
+++ app/(customer)/layout.tsx
@@ -4,24 +4,21 @@ import "../globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
-export default function RootLayout({
+export default function CustomerLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="vi">
-      <body>
-        {/* Header luôn nằm trên cùng */}
-        <Header />
+    <>
+      {/* Header luôn nằm trên cùng */}
+      <Header />
 
-        {/* Nội dung chính của từng trang sẽ nằm ở đây */}
-        {/* flex-grow giúp đẩy Footer xuống dưới cùng kể cả khi nội dung ngắn */}
-        <main className="grow">{children}</main>
+      {/* Nội dung chính của từng trang sẽ nằm ở đây */}
+      {children}
 
-        {/* Footer luôn nằm dưới cùng */}
-        <Footer />
-      </body>
-    </html>
+      {/* Footer luôn nằm dưới cùng */}
+      <Footer />
+    </>
   );
 }
